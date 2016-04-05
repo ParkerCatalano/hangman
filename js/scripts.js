8fd33002b1a6f424de5c92fdf0c281208cf19efe
Scott McIntire <scottmcintire90@gmail.com>
2016-04-04 20:44:17 -0700

    add the guessed letter and replace with _

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,28 @@
 $(document).ready(function() {
   $("form#player1-input").submit(function(event) {
     var inputtedWord = $("input#player1-word").val();
+    var splitWord = inputtedWord.split("");
+    var emptyArray = [];
+    for (var i = 0; i < splitWord.length; i++) {
+      splitWord[i].push("_")
+      $("#wordToGuess").text(splitWord[i]);
+      debugger;
+    }
+
     $("#player1-input").hide();
     $("#gallows").show();
     $("#player2-forms").show();
 
 event.preventDefault();
+  });
+
+  $("form#player2-input").submit(function(event) {
+    var guessedLetter = $("input#player2-letter").val();
+    $("#guessed").append(guessedLetter + "     ");
+    $("input#player2-letter").val("");
+    $("input#player2-word").val("");
+
+    event.preventDefault();
   });
 
 
